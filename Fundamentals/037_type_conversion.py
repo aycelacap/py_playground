183bb06b02c6e7b072c2792637f4162881c476bc
ayce <raycel5lacap@gmail.com>
2021-02-05 22:38:06 -0800

    Add to built in functions and methods

---
+++ Fundamentals/037_type_conversion.py
@@ -0,0 +1,13 @@
+name = "Ayce"
+age = 100
+relationship_status = "complicated"
+relationship_status = "single"
+
+# create a program that can guess your age
+
+birth_year = input("What year were you born?")
+guess = 2020 - int(birth_year) 
+
+print("your age is: {guess}")
+
+# string interpolation and input from user
