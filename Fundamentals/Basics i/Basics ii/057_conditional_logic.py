383dbda2101a2496e209cd91760347ce868a4713
ayce <raycel5lacap@gmail.com>
2021-02-25 18:01:36 -0800

    Elif

--- Fundamentals/Basics i/Basics ii/057_conditional_logic.py
+++ Fundamentals/Basics i/Basics ii/057_conditional_logic.py
@@ -1,4 +1,26 @@
 #  True || False
 
-# bumbumbum
+is_old = True
+is_licensed = True
+
+if is_old:
+  print('you are old enough to drive')
+elif is_licensed:
+  print('you can drive now') 
+else:
+  print('you are not of age')
+
+print('okokok')
+
+
+# if -> this condition is true,
+# we run the following code
+
+# we are able to skip lines and the interepreter will only continue with the following line 
+# if the condition is met
+
+# we can control the flow of the code
+
+# if the condition of if and elif is false, we run else 
+
 
