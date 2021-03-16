fbbcb8cc32c6f06357fc316a3ccf75290aa89b38
ayce <raycel5lacap@gmail.com>
2021-03-15 20:52:50 -0700

    Change rb file

--- odd_sums.rb
+++ odd_sums.rb
@@ -1,29 +1,27 @@
 # ODD SUMS
 # js presentations
 ​
-# Write a method that takes an array of numbers and returns the sum of all
-# odd numbers in the array.
-#
-# The method is expected to return an INTEGER
-# The method accepts a 'numbers' array as a parameter
 
-# create a counter that adds the odd numbers
-# iterate over the numbers array
-  # add conditional
 
-  # return the odd sum by the end 
+
 ​
 def odd_sums(numbers)
 #     odd_sum = 0
 # ​
 #     numbers.each do |num|
 #         if num % 2 != 0
-#             odd_sum += num
+#             odd_sum += num //13
 #         end
 #     end
 # ​
-#     return odd_sum
+#     return odd_sum // 13
 end
 ​
 ​
 puts odd_sums([3, 6, 8, 9, 1]) # => 13
+
+# Write a method that takes an array of numbers and returns the sum of all
+# odd numbers in the array.
+#
+# The method is expected to return an INTEGER
+# The method accepts a 'numbers' array as a parameter
