8be65e4202515d99dda01a6f67de16261a82f96a
ayce <raycel5lacap@gmail.com>
2021-03-22 22:21:12 -0700

    Add react instructions

---
+++ leetleet/react.js
@@ -0,0 +1,10 @@
+// [ X ] Query a list of 37 images
+// [ X ] Display the images in rows, 4 images per row
+//     // LEARN FKING GRIDS
+// [ X ] Make all the images the same size
+// [ X ] Instead of loading all images at once, create a "load more" button. Initial page loads 16 images, once load more is clicked 16 more get loaded in.
+// [ X ] Label each image with its author
+// [ X ] Make the images collapse into one column and centered in mobile view
+// [ X ] Add CSS effect on hover of an image, zoom it inwards
+// [ X ] Create an input to search for specific author
+// [  ] Create an X button on each image to delete that image from the UI
