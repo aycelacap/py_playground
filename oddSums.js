4b7d5fb9ce1bb4535bd3d41483dd5d31b60c2ce3
ayce <raycel5lacap@gmail.com>
2021-04-16 22:37:13 -0700

    Change JS for BCP

--- oddSums.js
+++ oddSums.js
@@ -4,9 +4,17 @@
   // if so, add to sum variable 
 // return the sum
 
-function oddSums(numbers) {
+function oddSums(numbers) { // [3, 6, 8, 9, 1]
   // this is where you code
+  let sum = 0;
+
+  for (let i = 0; i < numbers.length; i++) {
+    if (numbers[i] % 2 != 0) {
+      sum += numbers[i] // 3 + 9 + 1 = 13
+    }
+  }
   
+  return sum; // 13
 }
 
 
--- odd_sums.rb
+++ odd_sums.rb
@@ -8,15 +8,12 @@
 # The method accepts a 'numbers' array as a parameter
 
 def odd_sums(numbers)
-  # init a var for sum
-  # iterate over num array
-  # have a condition for finding odd numbers
-  # add those numbers to sum
-  # return 
+
+
   
 end
 
-
+puts odd_sums([3, 6, 8, 9, 1]) # => 13
 
 
 
@@ -25,12 +22,12 @@ end
 
 
 ​
-def odd_sums(numbers) #[3, 6, 8, 9, 1]
+def odd_sums(numbers) # [3, 6, 8, 9, 1]
     odd_sum = 0
 ​
     numbers.each do |num|
         if num % 2 != 0
-            odd_sum += num #13
+            odd_sum += num # 13
         end
     end
 ​
@@ -38,5 +35,5 @@ def odd_sums(numbers) #[3, 6, 8, 9, 1]
 end
 ​
 ​
-puts odd_sums([3, 6, 8, 9, 1]) # => 13
+
 
