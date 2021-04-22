53fa47e7c9979b6bbc77ed13b42ecd692c40ac44
ayce <raycel5lacap@gmail.com>
2021-04-21 21:27:31 -0700

    Change Angrams

--- leetleet/anagrams.js
+++ leetleet/anagrams.js
@@ -7,3 +7,23 @@
 // words = ["yo", "act", "flop", "tac", "foo", "cat", "oy", "olfp"]
 
 // Output = [["yo", "oy"], ["flop", "olfp"], ["act", "tac", "cat"], ["foo"]]
+
+// strategy
+// iterate nested loop
+// if the lengths of word1 is the same as word two feed it to helper
+
+function anagrams(words) {
+  let anagrams = [];
+
+  for (let i = 0; i < words.length; i++) {
+    let word1 = words[i]
+    for (let j = i; j < words.length - 1; j++) {
+      let word2 = words[j];
+
+      if (word1.sort === word2.sort) {
+        anagrams.push([word2]);
+      }
+    }
+  }
+  return anagrams;
+}
