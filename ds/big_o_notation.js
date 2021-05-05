b921b7ffb21cd39aaaa885581ae58d497f9c8c42
ayce <raycel5lacap@gmail.com>
2021-05-04 21:20:51 -0700

    Asymptotic analysis

--- ds/big_o_notation.js
+++ ds/big_o_notation.js
@@ -1,9 +1,19 @@
 // time complexity -> how much time it takes for it to be solved
-// space complexoty -> how much memory 
+// space complexoty -> how much auxilliary memory an algoorithm takes
 
 // time complexity depends on the size of the input 
 // we care about how the run time changes as the size of the input increases
 
-// Constant O(1)-> as sixe of the imput increases, the speed of n is constant
+// Constant O(1)-> as size of the input(n) increases, the speed of n is constant
 
-//Linearly 
+// Linearly O(N) -> as input increases, the speed of the algorithm increases linearly
+// were gonna do as many iterations as there is n
+
+// Pairing function -> n iterations for every n -> O(N^2) 
+
+// Asymptotic Analysis -> study the behaviour of a function as n approaches infinity
+
+// /////// constant ///////////
+// we dont care about elementary constants
+
+
